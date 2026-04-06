cecf9fd84ef538a31b8e0559685c5cc273fbceaf
valwango <valeriewang217@gmail.com>
2026-04-05 17:27:56 -0700

    one more

--- asset-version.js
+++ asset-version.js
@@ -1,4 +1,4 @@
-window.ASSET_VERSION = "20260405g";
+window.ASSET_VERSION = "20260405h";
 window.assetUrl = function assetUrl(path) {
   const normalizedPath = /^(\.\/|\.\.\/|\/|https?:)/.test(path) ? path : `./${path}`;
   const separator = normalizedPath.includes("?") ? "&" : "?";
