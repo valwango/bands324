8c4ac7d9a5609a6002a9f563360a232c5c340e2e
valwango <valeriewang217@gmail.com>
2026-04-05 18:58:20 -0700

    github refresh

--- asset-version.js
+++ asset-version.js
@@ -1,4 +1,4 @@
-window.ASSET_VERSION = "20260405m";
+window.ASSET_VERSION = "20260405n";
 window.assetUrl = function assetUrl(path) {
   const normalizedPath = /^(\.\/|\.\.\/|\/|https?:)/.test(path) ? path : `./${path}`;
   const separator = normalizedPath.includes("?") ? "&" : "?";
