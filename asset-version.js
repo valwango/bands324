3709dfb2a8bf3fe7b073cc1bcdfe0bb84615526e
valwango <valeriewang217@gmail.com>
2026-04-05 17:26:29 -0700

    trying again

--- asset-version.js
+++ asset-version.js
@@ -1,4 +1,4 @@
-window.ASSET_VERSION = "20260405f";
+window.ASSET_VERSION = "20260405g";
 window.assetUrl = function assetUrl(path) {
   const normalizedPath = /^(\.\/|\.\.\/|\/|https?:)/.test(path) ? path : `./${path}`;
   const separator = normalizedPath.includes("?") ? "&" : "?";
