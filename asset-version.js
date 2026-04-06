809549cd795a42d2937bf2c0ee33e1031edc4fc7
valwango <valeriewang217@gmail.com>
2026-04-05 17:16:36 -0700

    cntd

--- asset-version.js
+++ asset-version.js
@@ -1,4 +1,4 @@
-window.ASSET_VERSION = "20260405d";
+window.ASSET_VERSION = "20260405e";
 window.assetUrl = function assetUrl(path) {
   const separator = path.includes("?") ? "&" : "?";
   return `${path}${separator}v=${window.ASSET_VERSION}`;
