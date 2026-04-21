fc4eea776a345fc34bff312adbb5defd3153fe1d
valwango <valeriewang217@gmail.com>
2026-04-20 17:45:42 -0700

    mobilestats1

--- asset-version.js
+++ asset-version.js
@@ -1,4 +1,4 @@
-window.ASSET_VERSION = "20260405s";
+window.ASSET_VERSION = "20260420a";
 window.assetUrl = function assetUrl(path) {
   const normalizedPath = /^(\.\/|\.\.\/|\/|https?:)/.test(path) ? path : `./${path}`;
   const separator = normalizedPath.includes("?") ? "&" : "?";
