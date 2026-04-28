1c86874d1fd52945f871ca1578a32d70ff4dfb4a
valwango <valeriewang217@gmail.com>
2026-04-27 18:30:15 -0700

    a lil bigger

--- asset-version.js
+++ asset-version.js
@@ -1,4 +1,4 @@
-window.ASSET_VERSION = "20260427h";
+window.ASSET_VERSION = "20260427i";
 window.assetUrl = function assetUrl(path) {
   const normalizedPath = /^(\.\/|\.\.\/|\/|https?:)/.test(path) ? path : `./${path}`;
   const separator = normalizedPath.includes("?") ? "&" : "?";
