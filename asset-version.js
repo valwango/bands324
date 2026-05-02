3ab41dbd11d098c156feac0383e6d26aee7c9342
valwango <valeriewang217@gmail.com>
2026-05-02 10:18:20 -0700

    past dates move

--- asset-version.js
+++ asset-version.js
@@ -1,4 +1,4 @@
-window.ASSET_VERSION = "20260502h";
+window.ASSET_VERSION = "20260502i";
 window.assetUrl = function assetUrl(path) {
   const normalizedPath = /^(\.\/|\.\.\/|\/|https?:)/.test(path) ? path : `./${path}`;
   const separator = normalizedPath.includes("?") ? "&" : "?";
