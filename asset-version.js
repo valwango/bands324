0d216ce9df879a4589da6310213dcfde72536756
valwango <valeriewang217@gmail.com>
2026-05-02 10:38:18 -0700

    star

--- asset-version.js
+++ asset-version.js
@@ -1,4 +1,4 @@
-window.ASSET_VERSION = "20260502m";
+window.ASSET_VERSION = "20260502o";
 window.assetUrl = function assetUrl(path) {
   const normalizedPath = /^(\.\/|\.\.\/|\/|https?:)/.test(path) ? path : `./${path}`;
   const separator = normalizedPath.includes("?") ? "&" : "?";
