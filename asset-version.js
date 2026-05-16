1cdf1d48772edb314178b62fe2d8c9173b3f5166
valwango <valeriewang217@gmail.com>
2026-05-15 22:53:23 -0700

    signup page: match login page design

--- asset-version.js
+++ asset-version.js
@@ -1,4 +1,4 @@
-window.ASSET_VERSION = "20260515w";
+window.ASSET_VERSION = "20260515x";
 window.assetUrl = function assetUrl(path) {
   const normalizedPath = /^(\.\/|\.\.\/|\/|https?:)/.test(path) ? path : `./${path}`;
   const separator = normalizedPath.includes("?") ? "&" : "?";
