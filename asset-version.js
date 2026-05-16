707a049bed418642fcbcb069d1aa9d156eddc512
valwango <valeriewang217@gmail.com>
2026-05-15 19:06:24 -0700

    stack: top 12px

--- asset-version.js
+++ asset-version.js
@@ -1,4 +1,4 @@
-window.ASSET_VERSION = "20260515g";
+window.ASSET_VERSION = "20260515h";
 window.assetUrl = function assetUrl(path) {
   const normalizedPath = /^(\.\/|\.\.\/|\/|https?:)/.test(path) ? path : `./${path}`;
   const separator = normalizedPath.includes("?") ? "&" : "?";
