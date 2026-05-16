934a8c4078868a091e59ff97df7af74983ee20db
valwango <valeriewang217@gmail.com>
2026-05-15 23:30:22 -0700

    header: rename bands to wripbands

--- asset-version.js
+++ asset-version.js
@@ -1,4 +1,4 @@
-window.ASSET_VERSION = "20260516f";
+window.ASSET_VERSION = "20260516g";
 window.assetUrl = function assetUrl(path) {
   const normalizedPath = /^(\.\/|\.\.\/|\/|https?:)/.test(path) ? path : `./${path}`;
   const separator = normalizedPath.includes("?") ? "&" : "?";
