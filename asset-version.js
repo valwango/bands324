87854b4a339ece95596ff9c262273824227f47bd
valwango <valeriewang217@gmail.com>
2026-05-15 23:07:01 -0700

    login/signup: ENTER hint is clickable submit button

--- asset-version.js
+++ asset-version.js
@@ -1,4 +1,4 @@
-window.ASSET_VERSION = "20260515z";
+window.ASSET_VERSION = "20260516a";
 window.assetUrl = function assetUrl(path) {
   const normalizedPath = /^(\.\/|\.\.\/|\/|https?:)/.test(path) ? path : `./${path}`;
   const separator = normalizedPath.includes("?") ? "&" : "?";
