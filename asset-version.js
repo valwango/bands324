6f926a91fc69f78600ad15601edd2535d490e42a
valwango <valeriewang217@gmail.com>
2026-05-15 23:33:27 -0700

    header: reduce title font size by 5px

--- asset-version.js
+++ asset-version.js
@@ -1,4 +1,4 @@
-window.ASSET_VERSION = "20260516g";
+window.ASSET_VERSION = "20260516h";
 window.assetUrl = function assetUrl(path) {
   const normalizedPath = /^(\.\/|\.\.\/|\/|https?:)/.test(path) ? path : `./${path}`;
   const separator = normalizedPath.includes("?") ? "&" : "?";
