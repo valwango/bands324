b1d17d773f50170c8638b8885bcbbc83ee99f71f
valwango <valeriewang217@gmail.com>
2026-05-15 21:48:25 -0700

    fomo card: no border-radius, transparent bg

--- asset-version.js
+++ asset-version.js
@@ -1,4 +1,4 @@
-window.ASSET_VERSION = "20260515q";
+window.ASSET_VERSION = "20260515r";
 window.assetUrl = function assetUrl(path) {
   const normalizedPath = /^(\.\/|\.\.\/|\/|https?:)/.test(path) ? path : `./${path}`;
   const separator = normalizedPath.includes("?") ? "&" : "?";
