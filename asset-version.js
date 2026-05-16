aca25b0042d61e736191422d84a3acd6c5492ea4
valwango <valeriewang217@gmail.com>
2026-05-16 16:50:24 -0700

    profile: increase page padding and section gap

--- asset-version.js
+++ asset-version.js
@@ -1,4 +1,4 @@
-window.ASSET_VERSION = "20260516j";
+window.ASSET_VERSION = "20260516k";
 window.assetUrl = function assetUrl(path) {
   const normalizedPath = /^(\.\/|\.\.\/|\/|https?:)/.test(path) ? path : `./${path}`;
   const separator = normalizedPath.includes("?") ? "&" : "?";
