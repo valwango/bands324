d84eae28db935487eedf40b8c8b54726545e07d5
valwango <valeriewang217@gmail.com>
2026-05-15 23:02:14 -0700

    login/signup: faint ENTER hint on password fields

--- asset-version.js
+++ asset-version.js
@@ -1,4 +1,4 @@
-window.ASSET_VERSION = "20260515y";
+window.ASSET_VERSION = "20260515z";
 window.assetUrl = function assetUrl(path) {
   const normalizedPath = /^(\.\/|\.\.\/|\/|https?:)/.test(path) ? path : `./${path}`;
   const separator = normalizedPath.includes("?") ? "&" : "?";
