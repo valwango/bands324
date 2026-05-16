7a2d8ff4282981aff2c60b041baaffa115332c14
valwango <valeriewang217@gmail.com>
2026-05-15 23:08:06 -0700

    login/signup: fix ENTER button vertical centering

--- asset-version.js
+++ asset-version.js
@@ -1,4 +1,4 @@
-window.ASSET_VERSION = "20260516a";
+window.ASSET_VERSION = "20260516b";
 window.assetUrl = function assetUrl(path) {
   const normalizedPath = /^(\.\/|\.\.\/|\/|https?:)/.test(path) ? path : `./${path}`;
   const separator = normalizedPath.includes("?") ? "&" : "?";
